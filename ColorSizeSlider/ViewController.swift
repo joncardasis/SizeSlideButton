8130d37b0f748410e273374c8fe67703f51566bd
Cardasis , Jonathan (J.) <jcardasi@mgc000001684.fmccrf.ford.com>
2016-06-30 17:18:58 -0400

    Initial Commit

---
+++ ColorSizeSlider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ColorSizeSlider
+//
+//  Created by Cardasis, Jonathan (J.) on 6/30/16.
+//  Copyright © 2016 Cardasis, Jonathan (J.). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
